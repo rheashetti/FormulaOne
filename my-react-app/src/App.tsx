f1c4fcae4f3871cdf49ca06a67edd1824443f5ea
Rhea Shetti <shettir@uci.edu>
2025-02-22 20:33:24 -0800

    created nav bar

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className = "navBar">
+        <h1>Formula One Hub</h1>
+        <div className = "navBar-links">
+          <a href = "#home">Home</a>
+          <a href = "#blog">Race Recaps</a>
+          <a href = "#chabot">Formula 101</a>
+          <a href = "#drivers">Driver Performance</a>
+          <a href = "#pitstop">Pit Stop Performance</a>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className = "home">
+        <p>Welcome to the Formula One Hub! This is your one stop shop for all things Formula One.</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
