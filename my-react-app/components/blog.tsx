81bb39fc7ae94a63b97d39bb04b958888f6e1594
Rhea Shetti <shettir@uci.edu>
2025-04-20 20:32:17 -0700

    adding recaps

--- my-react-app/components/blog.tsx
+++ my-react-app/components/blog.tsx
@@ -1,29 +1,31 @@
 import React from 'react';
 import './blog.css';
 
+import saudiaarabia from './recaps/5-saudiarabia.jpg';
+import australia from './recaps/1-australia.jpg';
+import china from './recaps/2-china.jpg';
+import japan from './recaps/3-japan.jpg';
+import bahrain from './recaps/4-bahrain.jpg';
+
 const Blog = () => {
 
     const recaps = [
-        { id: 1, title:'Recap 1', content: 'This is the first recap' },
-        { id: 2, title:'Recap 2', content: 'This is the second recap' },
-        { id: 3, title:'Recap 3', content: 'This is the third recap' },
-        { id: 4, title:'Recap 4', content: 'This is the fourth recap' },
+        { id: 5, title:'Recap 1', content:  saudiaarabia},
+        { id: 4, title:'Recap 5', content: bahrain },
+        { id: 3, title:'Recap 4', content: japan },
+        { id: 2, title:'Recap 3', content: china },
+        { id: 1, title:'Recap 2', content: australia },
     ];
 
-    const mostRecentRecap = recaps[recaps.length - 1];
-    const otherRecaps = recaps.slice(0, recaps.length - 1);
+    const otherRecaps = recaps;
 
     return (
         <div className="blog-container">
-            <div className="most-recent-recap">
-                <h2>{mostRecentRecap.title}</h2>
-                <p>{mostRecentRecap.content}</p>
-            </div>
             <div className="recaps-grid">
                 {otherRecaps.map((recap) => (
                     <div key={recap.id} className="recap-card">
-                        <h3>{recap.title}</h3>
-                        <p>{recap.content}</p>
+                        {/* <h3>{recap.title}</h3> */}
+                        <img src={recap.content} alt={recap.title} />
                     </div>
                 ))}
             </div>
---
+++ my-react-app/components/drivers.tsx
@@ -0,0 +1,13 @@
+import "./drivers.css";
+import React from "react";
+
+const Drivers = () => {
+
+    return (
+        <div className="predictor-container">
+            
+        </div>
+    )
+}
+
+export default Drivers;
